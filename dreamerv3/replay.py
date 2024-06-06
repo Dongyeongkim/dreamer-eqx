fe607f889a57491f25fb753bd30395ef40b1757b
Dongyeongkim <dongyeongkim33@gmail.com>
2024-06-07 00:25:38 +0900

    modification is now on-going

--- dreamerv3/replay.py
+++ dreamerv3/replay.py
@@ -1,51 +1,62 @@
-from queue import Queue
+import jax
+import einops
+import jax.numpy as jnp
+from collections import deque
+from jax.tree_util import tree_map
+
 from typing import Dict
 
-import jax.numpy as jp
 
 
 class ReplayBuffer:
-    def __init__(self, chunk_size=1024):
+    def __init__(self, desireable_key_and_dim, chunk_size=1024):
+        self.deskeydim = desireable_key_and_dim
         self.chunk_size = chunk_size
+        self.num_chunk = 0
 
         self.buffer = {}
         self.left = {}
-        self._top = None
 
-    def push(self, data: Dict[str, jp.ndarray], downgrade_dim=False):
+    def push(self, data: Dict[str, jnp.ndarray]):
+        data = tree_map(lambda x, y: self.transform2ds(x, y), (data, self.deskeydim))
         for k, v in data.items():
             if k in self.left and self.left[k] is not None:
-                if downgrade_dim:
-                    v = v.reshape(-1, *v.shape[2:])
-                data = jp.concatenate([self.left[k], v], axis=0)
+                chunk = jax.device_put(jnp.concatenate([self.left[k], v], axis=0), device=jax.devices("cpu")[0])
                 self.left[k] = None
             else:
-                data = v
+                chunk = v
 
             if k not in self.buffer:
-                self.buffer[k] = Queue()
+                self.buffer[k] = deque()
 
-            while len(data) >= self.chunk_size:
-                self.buffer[k].put(data[:self.chunk_size])
-                data = data[self.chunk_size:]
+            while len(chunk) >= self.chunk_size:
+                self.buffer[k].put(chunk[:self.chunk_size])
+                chunk = chunk[self.chunk_size:]
 
-            if len(data) > 0:
-                self.left[k] = data
+            if len(chunk) > 0:
+                self.left[k] = chunk
 
     def pop(self):
-        if self._top is not None:
-            ret = self._top
-            self._top = None
-            return ret
-
         data = {}
         for k, v in self.buffer.items():
             if not v.empty():
-                data[k] = v.get()
+                data[k] = jax.device_put(v.popleft(), device=jax.devices()[0])
             else:
                 return None
         return data
-
-    def top(self):
-        self._top = self.pop()
-        return self._top
+    
+    def transform2ds(self, data: jnp.array, expected_dim: int):
+        # IMPORTANT: EXPECTED SHAPE -> (T, B, ...)
+        assert len(data.shape) < expected_dim + 2, " dimension cannot be bigger than expected shape + batch dimension"
+        assert len(data.shape) > expected_dim - 1, " dimension cannot be smaller than expected shape"
+        if len(data.shape) == expected_dim:
+            return data
+        elif len(data.shape) == expected_dim + 1:
+            return einops.rearrange(data, 't b ... -> (t b) ...')
+        else:
+            raise NotImplementedError("Something is wrong")
+    
+    def transform2batch(self, data: jnp.array, expected_dim: int, batch_size: int):
+        assert len(data.shape) == expected_dim, "dimension does not fit with expected dim"
+        assert self.chunk_size % batch_size == 0
+        return einops.rearrange(data, '(t b) ... -> b t ...', b=batch_size, t=self.chunk_size//batch_size)
